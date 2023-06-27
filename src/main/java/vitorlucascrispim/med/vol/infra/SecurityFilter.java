480104f677cc1ed62c54fee3d3549f3a001249eb
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-06-27 19:44:59 -0300

    Adicionando o filter, que realiza antes de todas as requisições a verificação se o token está presente na requisição

---
+++ src/main/java/vitorlucascrispim/med/vol/infra/SecurityFilter.java
@@ -0,0 +1,32 @@
+package vitorlucascrispim.med.vol.infra;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter  extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null){
+            throw new RuntimeException("Token não enviado");
+        }
+
+        return authHeader.replace("Bearer","");
+    }
+}
