99e4585e42acd09520b46300f8b766417ff2d2c2
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-06-27 18:28:49 -0300

    Finalizando as configurações de segurança e realizando um login com sucesso

--- src/main/java/vitorlucascrispim/med/vol/controllers/UsuarioController.java
+++ src/main/java/vitorlucascrispim/med/vol/controllers/UsuarioController.java
@@ -1,8 +1,32 @@
 package vitorlucascrispim.med.vol.controllers;
 
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vitorlucascrispim.med.vol.dtos.user.DadosAutenticacaoDTO;
 
-@Controller
+@RestController
+@RequestMapping("/login")
 public class UsuarioController {
+
+    @Autowired
+    private AuthenticationManager manager;
+    @PostMapping
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacaoDTO dadosAutenticacao){
+        var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(),dadosAutenticacao.senha());
+        var auth =  manager.authenticate(token);
+
+
+        return ResponseEntity.ok().build();
+
+    }
+
 }
---
+++ src/main/java/vitorlucascrispim/med/vol/dtos/user/DadosAutenticacaoDTO.java
@@ -0,0 +1,4 @@
+package vitorlucascrispim.med.vol.dtos.user;
+
+public record DadosAutenticacaoDTO(String login, String senha) {
+}
